13d33043b4911c5d9d49214c79d6cbaa844f452f
bolongcheng <cheng.bolong@gmail.com>
2017-04-13 01:38:23 -0700

    Variable name change in matlab file for consistency

--- svd_approx_partitionLS.m
+++ svd_approx_partitionLS.m
@@ -25,20 +25,20 @@ f = zeros(size_row,ranks_rows,ranks_cols); %the x vector, for each submatrix
 g = zeros(size_col,ranks_rows,ranks_cols); %the y vector, for each submatrix
 
 sample_VF = reshape(sample_VF,sample_size,[]); %reshape to sample_size x num_of_sub
-shift = min(-min(sample_VF(:))+1,0) + shift; 
+shift = min(-min(sample_VF(:))+1,0) + shift;
 sample_VF = sample_VF + shift(1,1); %use the same shift value for all submatrices
-b = log(sample_VF)./log(1.0001); 
+log_base = 1.0001;
+b = log(sample_VF)./log(log_base);
 a_b = LeastSq * b;  % a_b is the predicted values for all submatrices
 
-
-f_part = 1.0001.^(a_b(1 : size_row,:)); %x vector for submatrix
-g_part = 1.0001.^(a_b(size_row+1 : num_elements_part,:));%y vector for submatrix
+f_part = log_base.^(a_b(1 : size_row,:)); %x vector for submatrix
+g_part = log_base.^(a_b(size_row+1 : num_elements_part,:));%y vector for submatrix
 
 k = 1;
 for rank2 = 1:ranks_cols
     for rank1 = 1:ranks_rows
-        f(:,rank1,rank2) =  f_part(:,k); 
-        g(:,rank1,rank2) = g_part(:,k); 
+        f(:,rank1,rank2) = f_part(:,k);
+        g(:,rank1,rank2) = g_part(:,k);
         k=k+1;
     end
 end
